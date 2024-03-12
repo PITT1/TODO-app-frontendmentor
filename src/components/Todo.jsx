d462d148dc71e6285d8f576eba3d8773a0ed97ab
PITT1 <petroleo.plta.97@gmail.com>
2024-03-12 13:08:24 -0400

    footer listo solo falta reactividad

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -43,11 +43,20 @@ export const Todo = () => {
                                 item.map((item, index) => {
                                     const itemClass = index > 0 ? "border-item" : "";
                                     return(
-                                    <li key={index} className={`${itemClass} px-4 py-4 font-josefin text-custom-light-theme-Light-Grayish-Blue`}>{item}</li>
-                                )
-                            })
-                            }
+                                    <li key={index} className={`${itemClass} cursor-pointer px-4 py-4 font-josefin text-custom-light-theme-Light-Grayish-Blue`}>{item}</li>
+                                    )
+                                })}
                             </ul>
+                            <footer className=" relative border-item font-josefin text-custom-light-theme-Very-Dark-Grayish-Blue flex justify-between text-sm">
+                                <p className="pl-5 py-3">5 items left</p>
+                                <div className="text-lg rounded-lg sm:text-sm py-3 flex justify-center gap-3 absolute top-[140%] left-0 w-full bg-custom-dark-theme-Very-Dark-Desaturated-Blue sm:relative sm:w-auto">
+                                    <button className="hover:text-custom-dark-theme-Light-Grayish-Blue focus:text-custom-Bright-Blue">all</button>
+                                    <button className="hover:text-custom-dark-theme-Light-Grayish-Blue focus:text-custom-Bright-Blue">active</button>
+                                    <button className="hover:text-custom-dark-theme-Light-Grayish-Blue focus:text-custom-Bright-Blue transition-all">completed</button>
+                                </div>
+                                <button className="pr-5 py-3 hover:text-custom-dark-theme-Light-Grayish-Blue transition-all">clear completed</button>
+                                <h3 className="absolute top-[340%] sm:top-[200%] left-0 w-full text-center">Drag and drop to reorder list</h3>
+                            </footer>
                         </div>
                     </div>
                 </div>
